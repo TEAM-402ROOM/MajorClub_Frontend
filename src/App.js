dcd9925e67c979b25ecd7aa19ffeb07116380374
useonguk <ysu0219@gmail.com>
2023-11-21 10:31:31 +0900

    ADD:: application page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./reset.css";
-import { Header, Main, Report } from "./components/index";
+import { Header, Main, Report, Application } from "./components/index";
 
 const App = () => {
   return (
@@ -11,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Main />}></Route>
           <Route path="/report" element={<Report />}></Route>
+          <Route path="/application" element={<Application />}></Route>
           <Route path="*" element={<>404</>}></Route>
         </Routes>
       </BrowserRouter>
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,4 @@
 export { default as Header } from "./header/header";
 export { default as Main } from "./main/index";
 export { default as Report } from "./report/report";
+export { default as Application } from "./application/index";
