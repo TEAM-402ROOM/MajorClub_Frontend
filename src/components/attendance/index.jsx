70110bf5b5626a1a461b35da8f881fcf27f67f03
NameIsNick <ysu0219@gmail.com>
2024-01-21 18:50:51 +0900

    ADD:: page content 추가

--- src/components/attendance/index.jsx
+++ src/components/attendance/index.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import * as S from "./style";
 
 const Attendance = () => {
-  return <S.PageBox></S.PageBox>;
+  return (
+    <S.PageBox>
+      <S.PageTitle>📋 출석부</S.PageTitle>
+      <S.PageContent>
+        현재 소속된 동아리인 INSERT의 출석 현황이에요.
+      </S.PageContent>
+    </S.PageBox>
+  );
 };
 
 export default Attendance;
--- src/components/attendance/style.js
+++ src/components/attendance/style.js
@@ -3,3 +3,21 @@ import styled from "styled-components";
 export const PageBox = styled.div`
   padding: 0 175px;
 `;
+
+export const PageTitle = styled.p`
+  color: #000;
+  font-family: Pretendard;
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
+export const PageContent = styled.p`
+  color: #000;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 24px; /* 150% */
+`;
